fcdca9d29f5299c703f7e510db72fcb3372596a3
SaravanaPriyan07 <u.saravanapriyan@gmail.com>
2024-02-22 01:43:47 +0530

    first commit

---
+++ resume.js
@@ -0,0 +1,45 @@
+// About
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+  for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link")
+  document.getElementById(tabname).classList.add("active-tab")
+}
+
+// navBar
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";  
+}
+
+function closemenu() {
+  sidemenu.style.right = "-200px";  
+}
+
+// Contact
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwtdAQ308nx-xRfh91gOd5xoiR2ZB2hnHPxuAnB4Aw_9wPgOJ9vfM9K8bz7-SpDUwwm/exec'
+  const form = document.forms['submit-to-google-sheet'];
+  const msg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Thank You for the feedback."
+        setTimeout(function(){
+          msg.innerHTML = '';
+        },5000)
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
